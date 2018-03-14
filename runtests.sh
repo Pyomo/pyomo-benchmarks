0be5eedc2bc8e75299e71fa90d5985114a9557ec
William Hart <whart222@gmail.com>
2018-03-14 08:29:50 -0600

    Forcing removal of old results

--- runtests.sh
+++ runtests.sh
@@ -8,11 +8,11 @@ cd ..
 #
 # Test conversions
 #
-rm benchmarks/convertL_branches.csv
+rm -f benchmarks/convertL_branches.csv
 ./run --modules --branches convertL > convertL.out 2>&1
 cat benchmarks/convertL_branches.csv >> benchmarks/convertL_history.csv
 
-rm benchmarks/expr100000_branches.csv
+rm -f benchmarks/expr100000_branches.csv
 ./run --modules --branches expr100000 > expr100000.out 2>&1
 cat benchmarks/expr100000_branches.csv >> benchmarks/expr100000_history.csv
 
