75c42bc7f5d1933b29308c3148ef12209c84e403
William Hart <whart222@gmail.com>
2018-03-15 14:36:36 -0600

    Adding verbosity

--- benchmarks/convertL.py
+++ benchmarks/convertL.py
@@ -3,4 +3,4 @@ import sys
 
 R = 1
 
-convert.run(R, sys.argv[1], sys.argv[2], sys.argv[3], True, debug=False)
+convert.run(R, sys.argv[1], sys.argv[2], sys.argv[3], True, verbose=True, debug=False)
