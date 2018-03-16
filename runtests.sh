de00a412371b1126ab0b655d1824a9ec5f4af172
William Hart <whart222@gmail.com>
2018-03-16 08:29:51 -0600

    Adding verbosity

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -v
 #
 # Update installations of Pyomo versions
 #
@@ -8,14 +8,10 @@ cd ..
 #
 # Test conversions
 #
-#rm -f benchmarks/convertL_branches.csv
 ./run --modules convertL > convertL.out 2>&1
-#cat benchmarks/convertL_branches.csv >> benchmarks/convertL_history.csv
 ./summarize convertL
 
-#rm -f benchmarks/expr100000_branches.csv
 ./run --modules expr100000 > expr100000.out 2>&1
-#cat benchmarks/expr100000_branches.csv >> benchmarks/expr100000_history.csv
 ./summarize expr100000
 
 
