4bccfc9424c9de2a35bc3db16346b3e0b2e65e6a
William Hart <whart222@gmail.com>
2018-04-17 16:58:48 -0600

    Running long tests

--- runtests.sh
+++ runtests.sh
@@ -4,10 +4,10 @@
 #
 # Test conversions
 #
-./run --modules convertS > convertS.out 2>&1
-./summarize convertS
-#./run --modules convertL > convertL.out 2>&1
-#./summarize convertL
+#./run --modules convertS > convertS.out 2>&1
+#./summarize convertS
+./run --modules convertL > convertL.out 2>&1
+./summarize convertL
 
 #./run --modules expr100000 > expr100000.out 2>&1
 #./summarize expr100000
