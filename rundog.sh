d8c9aae835c8ab5ae494c5fb89b7c9e37d75e8d5
William Hart <whart222@gmail.com>
2018-04-18 11:24:24 -0600

    Updates to data processing.
    1. Adding text output in PDF files to explain
       what they are
    
    2. Adding dog16 - creates a CSV file for model
       construction and writing
    
    3. Move results into a 'results' directory

--- rundog.sh
+++ rundog.sh
@@ -3,9 +3,23 @@
 #echo "SKIPPING RUNDOG"
 #exit 0
 
+mkdir -p results
+
 ./dog 14 convertL
+mv dog14.pdf results/convertL_dog14.pdf
+
 ./dog 13 convertL
+mv dog13.pdf results/convertL_dog13.pdf
+
 ./dog 12 convertL
-./dog 10 convertL
+mv dog12.pdf results/convertL_dog12.pdf
+
+./dog 15 convertL
+mv dog15.csv results/convertL_dog15.csv
+
+./dog 16 convertL
+mv dog16.csv results/convertL_dog16.csv
+
 ./dog 20 convertL
+mv dog20.pdf results/convertL_dog20.pdf
 
