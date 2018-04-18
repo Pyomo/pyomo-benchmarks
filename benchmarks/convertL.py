b1ac75d679700dd02e7f1980846e16fab9756fbe
William Hart <whart222@gmail.com>
2018-04-18 00:03:57 -0600

    Adding more trials for baseline versions

--- benchmarks/convertL.py
+++ benchmarks/convertL.py
@@ -1,6 +1,12 @@
 import convert
 import sys
 
-R = 1
+#
+# We baseline non-releases with more trials
+#
+if sys.argv[3][0] in '0123456789':
+    R = 10
+else:
+    R = 1
 
 convert.run(R, sys.argv[1], sys.argv[2], sys.argv[3], True, verbose=True, debug=False)
