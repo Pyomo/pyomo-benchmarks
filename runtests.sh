6cb2f07be4b2edc4e7efcc28b234d56e5ec07108
William Hart <whart222@gmail.com>
2018-04-18 00:27:55 -0600

    Change to convertL tests

--- runtests.sh
+++ runtests.sh
@@ -7,13 +7,13 @@
 #
 # Test conversions
 #
-./run --modules --releases --keep convertS > convertS_releases.out 2>&1
-./run --modules convertS > convertS_branches.out 2>&1
-./summarize convertS
+#./run --modules --releases --keep convertS > convertS_releases.out 2>&1
+#./run --modules convertS > convertS_branches.out 2>&1
+#./summarize convertS
 
-#./run --modules --releases --keep convertL > convertL_releases.out 2>&1
-#./run --modules convertL > convertL.out 2>&1
-#./summarize convertL
+./run --modules --releases --keep convertL > convertL_releases.out 2>&1
+./run --modules convertL > convertL.out 2>&1
+./summarize convertL
 
 #./run --modules expr100000 > expr100000.out 2>&1
 #./summarize expr100000
