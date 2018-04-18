59837ad9d15c620e8083b186fdb7cdfe3e27e577
William Hart <whart222@gmail.com>
2018-04-18 09:25:28 -0600

    Graph generation for convertL

--- rundog.sh
+++ rundog.sh
@@ -3,9 +3,9 @@
 #echo "SKIPPING RUNDOG"
 #exit 0
 
-./dog 14 convertS
-./dog 13 convertS
-./dog 12 convertS
-./dog 10 convertS
-./dog 20 convertS
+./dog 14 convertL
+./dog 13 convertL
+./dog 12 convertL
+./dog 10 convertL
+./dog 20 convertL
 
