921353eca9b1ac82d1b1e111d7b9f6b0c24eda4d
William Hart <whart222@gmail.com>
2018-04-19 06:47:38 -0600

    Adding dog22 and dog23

--- rundog.sh
+++ rundog.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "SKIPPING RUNDOG"
-exit 0
+#echo "SKIPPING RUNDOG"
+#exit 0
 
 mkdir -p results
 
@@ -20,8 +20,18 @@ mv dog15.csv results/convertL_dog15.csv
 ./dog 16 convertL
 mv dog16.csv results/convertL_dog16.csv
 
-./dog 21 convertL
+./dog 21 convertL expr_dev
 mv dog21_pypy.pdf results/convertL_dog21_pypy.pdf
 mv dog21_python2.7.pdf results/convertL_dog21_python2.7.pdf
 mv dog21_python3.6.pdf results/convertL_dog21_python3.6.pdf
+
+./dog 22 convertL expr_dev
+mv dog22_expr_dev_pypy.pdf results/convertL_dog22_expr_dev_pypy.pdf
+mv dog22_expr_dev_python2.7.pdf results/convertL_dog22_expr_dev_python2.7.pdf
+mv dog22_expr_dev_python3.6.pdf results/convertL_dog22_expr_dev_python3.6.pdf
+
+./dog 23 convertL expr_dev
+mv dog23_expr_dev_pypy.pdf results/convertL_dog23_expr_dev_pypy.pdf
+mv dog23_expr_dev_python2.7.pdf results/convertL_dog23_expr_dev_python2.7.pdf
+mv dog23_expr_dev_python3.6.pdf results/convertL_dog23_expr_dev_python3.6.pdf
 
