68cac5e176fff6fd8214d922ca3f00aa6343bc63
William Hart <whart222@gmail.com>
2018-04-28 07:53:19 -0600

    Cleanup
    1. Remove graphs that are specific to the expr_dev branch
    
    2. dog22 and dog23 can include data from multiple branches

--- rundog.sh
+++ rundog.sh
@@ -5,33 +5,33 @@
 
 mkdir -p results
 
-./dog 14 convertL
-mv dog14.pdf results/convertL_dog14.pdf
-
 ./dog 13 convertL
 mv dog13.pdf results/convertL_dog13.pdf
 
 ./dog 12 convertL
 mv dog12.pdf results/convertL_dog12.pdf
 
-./dog 15 convertL
-mv dog15.csv results/convertL_dog15.csv
+#./dog 14 convertL
+#mv dog14.pdf results/convertL_dog14.pdf
 
-./dog 16 convertL
-mv dog16.csv results/convertL_dog16.csv
+#./dog 15 convertL
+#mv dog15.csv results/convertL_dog15.csv
 
-./dog 21 convertL expr_dev
+#./dog 16 convertL
+#mv dog16.csv results/convertL_dog16.csv
+
+./dog 21 convertL
 mv dog21_pypy.pdf results/convertL_dog21_pypy.pdf
 mv dog21_python2.7.pdf results/convertL_dog21_python2.7.pdf
 mv dog21_python3.6.pdf results/convertL_dog21_python3.6.pdf
 
-./dog 22 convertL expr_dev
-mv dog22_expr_dev_pypy.pdf results/convertL_dog22_expr_dev_pypy.pdf
-mv dog22_expr_dev_python2.7.pdf results/convertL_dog22_expr_dev_python2.7.pdf
-mv dog22_expr_dev_python3.6.pdf results/convertL_dog22_expr_dev_python3.6.pdf
+./dog 22 convertL # ADD BRANCHES HERE
+mv dog22_pypy.pdf results/convertL_dog22_pypy.pdf
+mv dog22_python2.7.pdf results/convertL_dog22_python2.7.pdf
+mv dog22_python3.6.pdf results/convertL_dog22_python3.6.pdf
 
-./dog 23 convertL expr_dev
-mv dog23_expr_dev_pypy.pdf results/convertL_dog23_expr_dev_pypy.pdf
-mv dog23_expr_dev_python2.7.pdf results/convertL_dog23_expr_dev_python2.7.pdf
-mv dog23_expr_dev_python3.6.pdf results/convertL_dog23_expr_dev_python3.6.pdf
+./dog 23 convertL # ADD BRANCHES HERE
+mv dog23_pypy.pdf results/convertL_dog23_pypy.pdf
+mv dog23_python2.7.pdf results/convertL_dog23_python2.7.pdf
+mv dog23_python3.6.pdf results/convertL_dog23_python3.6.pdf
 
