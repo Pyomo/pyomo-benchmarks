97e40bbea9de513cf839a29e240750a92f1e0144
William Hart <whart222@gmail.com>
2018-04-30 10:45:37 -0600

    Updat for command-line

--- benchmarks/convertL.py
+++ benchmarks/convertL.py
@@ -4,9 +4,16 @@ import sys
 #
 # We baseline releases with more trials
 #
-if sys.argv[3][0] in '0123456789':
+if len(sys.argv) == 4 and sys.argv[3][0] in '0123456789':
     R = 10
 else:
-    R = 1
+    R = 10
 
-convert.run(R, sys.argv[1], sys.argv[2], sys.argv[3], True, verbose=True, debug=False)
+if len(sys.argv) == 2:
+    import os
+    import platform
+    if os.path.exists(sys.argv[1]):
+        os.remove(sys.argv[1])
+    convert.run(R, sys.argv[1], "%s %s" % (platform.python_implementation(), platform.python_version()), 'trials', True, verbose=True, debug=False)
+else:
+    convert.run(R, sys.argv[1], sys.argv[2], sys.argv[3], True, verbose=True, debug=False)
