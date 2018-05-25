18a60084e3870cbffc16c34a12807c5321948950
William Hart <whart222@gmail.com>
2018-05-24 23:08:14 -0600

    Removing expr_not_variable

--- versions/install.py
+++ versions/install.py
@@ -13,7 +13,7 @@ import csv
 #
 # Testing specific branches
 #
-branches = ['master', 'expr_not_variable', 'expr_wo_asnumeric']
+branches = ['master', 'expr_wo_asnumeric']
 
 #                   PYOMO           PYUTILIB
 pyutilib_version = {
